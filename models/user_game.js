cf92621d7e9fcff3233ed4ae0dd9f157ca252398
ichwan mrz <ichwan.mrz@gmail.com>
2022-07-04 22:06:12 +0700

    commit chapter 6

---
+++ models/user_game.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user_game extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasOne(models.user_game_biodata, {foreignKey: "id_user"});
+    }
+  }
+  user_game.init({
+    // id_user: DataTypes.INTEGER,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    isSuperAdmin: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'user_game',
+  });
+  return user_game;
+};
